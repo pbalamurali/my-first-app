4ffdd4121feb9af30cf49f10e6179fff6128c6f5
pbalamurali <baladotcom@gmail.com>
2021-04-09 09:48:14 +0530

    Adding project files

---
+++ MyFirstApp/SecretsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstApp
+{
+    [ApiController]
+    public class SecretsController: ControllerBase
+    {
+
+        private readonly IConfiguration _configuration;
+
+        public SecretsController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [Route("api/secret")]
+        public string GetSecret()
+        {
+            return _configuration.GetValue<string>("secret");
+        }
+
+    }
+}
